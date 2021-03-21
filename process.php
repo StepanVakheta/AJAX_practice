0047c3e05c9eb4c5bfdc36d63cc8d81fae593d67
Stepan Vakheta <stepanvakheta@icloud.com>
2021-03-21 09:51:54 +0300

    placeholder for learning

--- process.php
+++ process.php
@@ -4,7 +4,6 @@ echo 'Processing...';
 
 // check for POST variable
 if(isset($_POST['name'])){
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
     echo 'POST: Your name is '. $_POST['name'];
 }
 
