49d4fa3e5a42796f156afc227e5246a5e69ea012
Stepan Vakheta <stepanvakheta@icloud.com>
2021-03-21 09:33:17 +0300

    placeholder for learning

--- process.php
+++ process.php
@@ -2,5 +2,14 @@
 
 echo 'Processing...';
 
+// check for POST variable
+if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    echo 'POST: Your name is '. $_POST['name'];
+}
 
+// check for GET variable
+if(isset($_GET['name'])){
+    echo 'GET: Your name is '. $_GET['name'];
+}
 
--- script-php.js
+++ script-php.js
@@ -1,12 +1,37 @@
 document.getElementById('button').addEventListener('click', getName)
+document.getElementById('getform').addEventListener('submit', getName)
+document.getElementById('postform').addEventListener('submit', postName)
 
-function getName() {
+function getName(e) {
+    
+    e.preventDefault()
+
+    var name = document.getElementById('nameget').value
+
     var xhr = new XMLHttpRequest()
-    xhr.open('GET', 'process.php', true)
+    xhr.open('GET', 'process.php?name=' + name, true)
 
     xhr.onload = function() {
         console.log(this.response);
     }
 
     xhr.send()
+}
+
+function postName(e) {
+    
+    e.preventDefault()
+
+    var name = document.getElementById('namepost').value
+    var params = 'name=' + name
+
+    var xhr = new XMLHttpRequest()
+    xhr.open('POST', 'process.php', true)
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+    xhr.onload = function() {
+        console.log(this.response);
+    }
+
+    xhr.send(params)
 }
